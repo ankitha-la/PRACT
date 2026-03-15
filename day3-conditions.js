20de352cccc02ba5db4fc59418e670d4d319fbfc
ankitha-la <ankithala24@gmailcom>
2026-03-15 13:34:45 +0530

    Day3 conditions practice

---
+++ day3-conditions.js
@@ -0,0 +1,34 @@
+//example1
+let number = 10;
+
+if(number%2==0)
+{
+    console.log(`${number} is even`);
+    
+}else{
+    console.log(`${number} is odd`);
+}
+
+//example2
+let age = 22
+if(age >18)
+{
+    console.log("eligible to vote");
+}
+else{
+    console.log("not eligible");
+    
+}
+
+// largest of two numbers
+
+let a = 25
+let b = 40
+
+if (a>b){
+    console.log(`${a} is greater`);
+    
+}else{
+    console.log(`${b} is greater`);
+    
+}
