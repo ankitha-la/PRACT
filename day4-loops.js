0b417d2a2d7c42abf169575ea63edb11548f6e9b
ankitha-la <ankithala24@gmailcom>
2026-03-15 21:35:24 +0530

    Day4 loops practice

---
+++ day4-loops.js
@@ -0,0 +1,42 @@
+// // for(i=1;i<=10;i++)
+// // {
+// //     console.log(i);
+    
+// // }
+
+
+// for(i=1;i<=20;i++)
+// {
+//     if(i%2===0){
+//         console.log(i);
+        
+//     }
+// }
+
+// let sum =0;
+// 
+// 
+// for(let i=0;i<=10;i++)
+// {
+    // sum = sum + i
+    // 
+// }
+// console.log(`sum =${sum}`);
+//
+
+
+let sum = 0;
+for(let i=1;i<=100;i++)
+{
+    sum = sum + i
+    
+}
+console.log(`sum =${sum}`);
+
+
+
+
+
+
+
+
