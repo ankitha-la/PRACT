c6002437043c486b0e1bf8b1e38ba41ae9588838
ankitha-la <ankithala24@gmailcom>
2026-03-15 12:26:03 +0530

    Day 2 operators practice

--- day2-operators.js
+++ day2-operators.js
@@ -1,12 +1,23 @@
-num1 = 15
-num2 = 5
+// num1 = 15
+// num2 = 5
+// 
+// console.log(`Addition =  ${num1+num2}`);
+// 
+// console.log(`Subtraction =  ${num1-num2}`);
+// 
+// console.log(`Multiplication =  ${num1*num2}`);
+// 
+// console.log(`Division  =  ${num1/num2}`);
+// 
+// console.log(`Remainder =  ${num1%num2}`);
+// 
 
-console.log(`Addition =  ${num1+num2}`);
+let math = 80
+let science = 70
+let english = 90
 
-console.log(`Subtraction =  ${num1-num2}`);
+let total = math+science+english
+console.log(`Total = ${total}`);
+let Average = (total)/3 ;
+console.log(`Average = ${Average}`);
 
-console.log(`Multiplication =  ${num1*num2}`);
-
-console.log(`Division  =  ${num1/num2}`);
-
-console.log(`Remainder =  ${num1%num2}`);
